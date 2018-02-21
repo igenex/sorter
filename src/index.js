e24c92293cdf12b5cdbe2314ee0a12d18c23b16a
Eugene <igenexxx@gmail.com>
2018-02-21 22:46:27 +0300

    Ugly, but working

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.sortType = (a, b) => a - b;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
+
   sort(indices) {
-    // your implementation
+    let temp_array = [];
+    indices.forEach(item => {
+      temp_array.push(this.array[item]);
+    });
+    temp_array.sort(this.sortType);
+    indices.sort((a, b) => a - b);
+    indices.forEach((item, i) => {
+      this.array[item] = temp_array[i];
+    });
   }
 
   setComparator(compareFunction) {
     // your implementation
+    if (typeof compareFunction === "function") {
+      this.sortType = compareFunction;
+    }
   }
 }
 
 module.exports = Sorter;
+
